4ea68fedfa604830a1cd43d5a018968585e141c0
mohalsha <engmohammadalsharif@gmail.com>
2024-06-27 09:34:11 +0300

    Update Day3Exce.py

--- Day3Exce.py
+++ Day3Exce.py
@@ -37,10 +37,27 @@ class Line:
 
 # Excercise 3
 
-class shape:
-    def __init__(self, line1, line2):
-        self.line1 = line1
-        self.line2 = line2
+import matplotlib.pyplot as plt
+class shape(Line):
+
+    def __init__(self,List):
+        self.List=List
+
+    def __str__(self):
+        return "Please enter the points to draw the shape"
+    
+    
+    def draw_lines(List):
+       for Line in List:
+           Line = ((x1, y1), (x2, y2))
+           plt.plot([x1, x2], [y1, y2],)
+    
+
+    #plt.show()
+
+list: [((0, 0), (1, 0)),((1, 0), (0.5, 1)),((0.5, 1), (0, 0))]
+    
+shape.draw_lines(list)
 
 
 
